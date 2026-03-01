779c2e889b98afa3d4b7bf2002516fba378dc939
Raadaas <80067819+Raadaas@users.noreply.github.com>
2026-03-01 20:25:57 +0100

    #23-Dodavanje-containera

--- eCommerce/eCommerce.Subscriber/Program.cs
+++ eCommerce/eCommerce.Subscriber/Program.cs
@@ -9,6 +9,7 @@ var config = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json", optional: true)
     .AddJsonFile("appsettings.Development.json", optional: true)
+    .AddEnvironmentVariables()
     .Build();
 
 var connectionString = config.GetConnectionString("DefaultConnection")
